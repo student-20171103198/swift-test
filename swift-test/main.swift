20fb732f7eb0afc0e8e5ee9810f9946ec6d994d3
s20171103198 <s20171103198@mac04.local>
2018-09-28 20:06:37 +0800

    Initial Commit

---
+++ swift-test/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  swift-test
+//
+//  Created by s20171103198 on 2018/9/28.
+//  Copyright © 2018年 s20171103198. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
